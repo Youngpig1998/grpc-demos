8c083008f606f388bff2109b6b13ccfcb77edeba
Youngpig1998 <873185434@qq.com>
2022-08-16 16:42:02 +0800

    first commit

---
+++ route-client/client.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/Youngpig1998/grpc-example/utils"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/Youngpig1998/grpc-example/route"
+	"google.golang.org/grpc"
+)
+
+func getFeature(client pb.RouteGuideClient) {
+	feature, err := client.GetFeature(context.Background(), &pb.Point{
+		Latitude:  310235000,
+		Longitude: 121437403,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(feature)
+}
+
+func listFeatures(client pb.RouteGuideClient) {
+	serverStream, err := client.ListFeatures(context.Background(), &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 313374060, Longitude: 121358540},
+		Hi: &pb.Point{Latitude: 311034130, Longitude: 121598790},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		feature, err := serverStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(feature)
+	}
+}
+
+func recordRoute(client pb.RouteGuideClient) {
+	// dummy data
+	points := []*pb.Point{
+		{Latitude: 313374060, Longitude: 121358540},
+		{Latitude: 311034130, Longitude: 121598790},
+		{Latitude: 310235000, Longitude: 121437403},
+	}
+
+	clientStream, err := client.RecordRoute(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, point := range points {
+		if err := clientStream.Send(point); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(time.Second)
+	}
+	summary, err := clientStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(summary)
+}
+
+func recommend(client pb.RouteGuideClient) {
+	stream, err := client.Recommend(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// this goroutine listen to the server stream
+	go func() {
+		for {
+			feature, err2 := stream.Recv()
+			if err2 == io.EOF {
+				break
+			}
+			if err2 != nil {
+				log.Fatalln(err2)
+			}
+			fmt.Println("Recommended: ", feature)
+		}
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		request := pb.RecommendationRequest{Point: new(pb.Point)}
+		var mode int32
+		fmt.Print("Enter Recommendation Mode (0 for farthest, 1 for the nearest)")
+		utils.ReadIntFromCommandLine(reader, &mode)
+		fmt.Print("Enter Latitude: ")
+		utils.ReadIntFromCommandLine(reader, &request.Point.Latitude)
+		fmt.Print("Enter Longitude: ")
+		utils.ReadIntFromCommandLine(reader, &request.Point.Longitude)
+		request.Mode = pb.RecommendationMode(mode)
+
+		if err := stream.Send(&request); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:5000", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalln("client cannot dial grpc server")
+	}
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+
+	getFeature(client)
+
+	//runForth(client)
+}
---
+++ route-server/server.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"github.com/Youngpig1998/grpc-example/utils"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/Youngpig1998/grpc-example/route"
+	"google.golang.org/grpc"
+)
+
+type routeGuideServer struct {
+	features []*pb.Feature //模拟一个数据库
+	pb.UnimplementedRouteGuideServer
+}
+
+func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
+	for _, feature := range s.features {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	return nil, nil
+}
+
+func (s *routeGuideServer) ListFeatures(rectangle *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.features {
+		if utils.InRange(feature.Location, rectangle) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
+	startTime := time.Now()
+	var pointCount, distance int32
+	var prevPoint *pb.Point
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			// conclude a route summary
+			endTime := time.Now()
+			return stream.SendAndClose(&pb.RouteSummary{
+				PointCount:  pointCount,
+				Distance:    distance,
+				ElapsedTime: int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+
+		pointCount++
+		if prevPoint != nil {
+			distance += utils.CalcDistance(prevPoint, point)
+		}
+		prevPoint = point
+	}
+
+	return nil
+}
+
+func (s *routeGuideServer) recommendOnce(request *pb.RecommendationRequest) (*pb.Feature, error) {
+	var nearest, farthest *pb.Feature
+	var nearestDistance, farthestDistance int32
+
+	for _, feature := range s.features {
+		distance := utils.CalcDistance(feature.Location, request.Point)
+		if nearest == nil || distance < nearestDistance {
+			nearestDistance = distance
+			nearest = feature
+		}
+		if farthest == nil || distance > farthestDistance {
+			farthestDistance = distance
+			farthest = feature
+		}
+	}
+	if request.Mode == pb.RecommendationMode_GetFarthest {
+		return farthest, nil
+	} else {
+		return nearest, nil
+	}
+}
+
+func (s *routeGuideServer) Recommend(stream pb.RouteGuide_RecommendServer) error {
+	for {
+		request, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		recommended, err := s.recommendOnce(request)
+		if err != nil {
+			return err
+		}
+		err = stream.Send(recommended)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// New一个routeGuideServer
+func newServer() *routeGuideServer {
+	return &routeGuideServer{
+		features: []*pb.Feature{
+			{Name: "上海交通大学闵行校区 上海市闵行区东川路800号", Location: &pb.Point{
+				Latitude:  310235000,
+				Longitude: 121437403,
+			}},
+			{Name: "复旦大学 上海市杨浦区五角场邯郸路220号", Location: &pb.Point{
+				Latitude:  312978870,
+				Longitude: 121503457,
+			}},
+			{Name: "华东理工大学 上海市徐汇区梅陇路130号", Location: &pb.Point{
+				Latitude:  311416130,
+				Longitude: 121424904,
+			}},
+		},
+	}
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", "localhost:5000")
+	if err != nil {
+		log.Fatalln("cannot create a listener at the address")
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	log.Fatalln(grpcServer.Serve(lis))
+}
